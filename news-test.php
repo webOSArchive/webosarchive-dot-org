5c7b6fe13621533d23d8c44e60ce93c65627886c
CodePoet80 <jonathan.m.wise@gmail.com>
2021-11-25 11:06:10 -0500

    cleanup

--- news-test.php
+++
@@ -1,9 +0,0 @@
-<style>
-    body,* { font-size: 14px; font-family: Helvetica, Arial, sans-serif }
-</style>
-<?php
-    $rssFeedURI="http://twitter.webosarchive.com/?1463262475361533952";
-?>
-<div style="min-width:200px; max-width: 600px; min-height: 200px; max-height: 300px; overflow-y:scroll; overflow-x:hidden">
-    <?php include("rssWidget.php") ?>
-</div>
