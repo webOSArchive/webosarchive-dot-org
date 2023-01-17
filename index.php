105a72aedc71e5a41c486d883813c626665d83b8
codepoet <jon.wise@jonandnic.com>
2023-01-17 14:17:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<?php
-if (strpos($_SERVER['HTTP_HOST'], "webosarchive.com") !== false) {
-    $url = "http://"; 
-    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
-         $url = "https://";
-    $url.= str_replace("webosarchive.com", "webosarchive.org", $_SERVER['HTTP_HOST']);
-    $url.= $_SERVER['REQUEST_URI'];
-    if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] != "")
-      $url.="?".$_SERVER['QUERY_STRING'] ;
-  header("Location: .$url);
-  die();
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
