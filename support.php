196fc131fafa7ad1633ae23d7f6d3e81d910ddb0
codepoet80 <curator@webosarchive.org>
2023-05-30 16:24:35 -0400

    add support page

---
+++ support.php
@@ -0,0 +1,42 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>webOS Archive</title>
+
+<link rel="stylesheet" href="wosa.css">
+<style>
+td { font-size: 14px; }
+</style>
+</head>
+<body style="margin:0px;" height="100%">
+
+<?php include('social-meta.php') ?>
+<?php include('menu.php')?>
+
+<div id="container">
+
+<div class="wosaTitle">
+  <div class="wosaIntro">
+    <h2>Support</h2>
+  </div>
+</div>
+<div class="wosaIntro">
+    <img class="circle-image" style="margin: -12px 14px 14px 14px; float:left" src="museum.png">
+    <p style="padding-top: 16px;">webOSArchive (WOSA) is the unofficial repository of information, restoration efforts, and archives for Palm/HP's mobile webOS operating system. This site does not provide material or information about the spin-off operating systems, webOS Open Source Edition (wOSE) or LG's webOS for TVs.</p>
+    <p>Its the position of the curator, and the remaining webOS community, that Palm and HP's webOS devices, including the Pre series phones, the Veer and Pixi phones, and the TouchPad, remain useful devices that both provide value to their users and education to the rest of the industry. In fact, many webOS innovations have been copied by modern mobile OS developers.</p>
+    <p>webOS Archive is run without profit, thanks to the contributions of volunteers. If you want to help, here's some ways to get involved:</p>
+    <ul>
+      <li><b>Financial Support:</b> Help keep the server and services up and running by donating cash via <a href="https://github.com/sponsors/webOSArchive" target="_blank">GitHub Sponsors</a> or <a href="https://patreon.com/webOSArchive" target="_blank">Patreon</a>.</li>
+      <li><b>Mirroring:</b> Museum contents are not hosted on the archive server, but on mirrors around the world to maximize availability. <a href="https://appcatalog.webosarchive.org/" target="_top">Contact the Curator</a> if you can help!</li>
+      <li><b>Coding:</b> If you can write code for the web, you can write code for webOS -- check out the <a href="https://sdk.webosarchive.org/" target="_top">SDK</a>, and reach out to the <a href="http://www.webosarchive.org/docs/community">Community</a> about how you can pitch in!</a>
+      <li><b>Documenting and Archival:</b> Lots of content has been preserved by the Wayback Machine, but much of it needs updating, or re-working for modern times. <a href="https://github.com/webosarchive/webos-docs">Pull Requests</a> are welcome!
+</div>
+
+<div class="wosaIntro" style="margin-top: 14px; margin-bottom: 40px; padding-bottom: 10px;">
+  <p style="font-size:smaller">The material on this site has been collected from private archives, the Internet Archive's wayback machine, and historical forum and blog posts and is preserved here for public interest under Fair Use provisions of United States' copyright law. The curator will respond promptly to formal and legally authorized take-down requests from rights holders who can demonstrate clear harm that results from having their historical material preserved.</p>
+</div>
+
+<footer>
+<?php include("legalese.php"); ?>
+</footer>
+</div>
+</body>
