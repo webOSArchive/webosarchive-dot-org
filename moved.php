452dd48a336d511a29ab29a68b9cb7030d90b64d
codepoet80 <jon.wise@jonandnic.com>
2023-08-21 15:26:02 -0400

    change script

--- moved.php
+++ moved.php
@@ -10,7 +10,7 @@
         if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] != "" && $_SERVER['QUERY_STRING'] != "/") {
             $q = $_SERVER["QUERY_STRING"];
             ?>
-            <script>setTimeout("document.location='https://www.webosarchive.com?<?php echo $q?>'", 2000)</script>
+            <script>setTimeout("document.location='https://www.webosarchive.org/<?php echo $q?>'", 2000)</script>
             <?php
         }
         ?>
