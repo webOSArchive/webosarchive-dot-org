5b7599f6a669a341d654a71ab01ee8c1b1ca20dc
codepoet <info@codepoetry.app>
2023-12-07 21:08:40 -0500

    change structure

--- news/index.php
+++ news/index.php
@@ -1,3 +1,42 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>webOS Archive</title>
+
+<link rel="stylesheet" href="../wosa.css">
+<style>
+
+</style>
+</head>
+<body style="margin:0px;" height="100%">
+
+<?php include('../social-meta.php') ?>
+<?php include('../menu.php')?>
 <?php
-header("Location: ../news.php");
+    //$rssFeedURI="http://twitter.webosarchive.org/?twitterid=MTQ2MzI2MjQ3NTM2MTUzMzk1Mg";
+    $rssFeedURI="https://palm.weboslives.eu/users/webosarchive.rss";
 ?>
+
+<div id="container" class="content">
+<img class="circle-image" style="" src="../museum.png">
+  <h1 style="">webOS Archive | News</h1>
+
+  <div style="margin-left: 10px; margin-right: 10px">
+    <p style="padding-top: 16px;">webOSArchive (WOSA) is the unofficial repository of information, restoration efforts, and archives for Palm/HP's mobile webOS operating system. This site does not provide material or information about the spin-off operating systems, webOS Open Source Edition (wOSE) or LG's webOS for TVs.</p>
+    <p style="text-align: right !important; font-weight:bold;">
+      Follow: 
+      <a href="https://palm.weboslives.eu/users/webosarchive">Mastodon</a> | 
+      <a href="<?php echo $rssFeedURI; ?>">RSS</a> |
+      <a href="https://www.twitter.com/webOSArchive">Twitter</a>
+    </p>
+    <div>
+        <?php include("../rssWidget.php") ?>
+    </div>
+
+  </div>
+</div>
+
+<footer>
+<?php include("../legalese.php"); ?>
+</footer>
+</div>
+</body>
--- support/index.php
+++ support/index.php
@@ -1,3 +1,63 @@
-<?php
-header("Location: ../support.php");
-?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>webOS Archive</title>
+
+<link rel="stylesheet" href="../wosa.css">
+<style>
+
+</style>
+</head>
+<body style="margin:0px;" height="100%">
+
+<?php include('../social-meta.php') ?>
+<?php include('../menu.php')?>
+
+<div id="container" class="content">
+<img class="circle-image" style="" src="../museum.png">
+  <h1 style="">webOS Archive | Support Us</h1>
+
+  <div style="margin-left: 10px; margin-right: 10px">
+    <p style="padding-top: 16px;">webOSArchive (WOSA) is the unofficial repository of information, restoration efforts, and archives for Palm/HP's mobile webOS operating system. This site does not provide material or information about the spin-off operating systems, webOS Open Source Edition (wOSE) or LG's webOS for TVs.</p>
+    <p>Its the position of the curator, and the remaining webOS community, that Palm and HP's webOS devices, including the Pre series phones, the Veer and Pixi phones, and the TouchPad, remain useful devices that both provide value to their users and education to the rest of the industry. In fact, many webOS innovations have been copied by modern mobile OS developers.</p>
+    <p>webOS Archive is run without profit, thanks to the contributions of volunteers. If you want to help, here's some ways to get involved:</p>
+    <div class="feature-block">
+      <span class="feature-title">Financial Support</span>
+      <div class="feature-content">
+        Help keep the server and services up and running by donating cash...
+        <ul>
+          <li><a href="https://github.com/sponsors/webOSArchive" target="_blank">GitHub Sponsors</a></li>
+          <li><a href="https://patreon.com/webOSArchive" target="_blank">Patreon</a></li>
+          <li><a href="https://ko-fi.com/webosarchive" target="_blank">Ko-Fi</a></li>
+          <li><a href="https://play.google.com/store/apps/details?id=eu.weboslives.webostracker" target="_blank">Buy our app on Google Play!</a></li>
+        </ul>
+      </div>  
+    </div>
+
+    <div class="feature-block">
+      <span class="feature-title">Content Mirroring</span>
+      <div class="feature-content">
+      Museum contents are not hosted on the archive server, but on mirrors around the world to maximize availability. <a href="https://appcatalog.webosarchive.org/" target="_top">Contact the Curator</a> if you can help!
+      </div>  
+    </div>
+
+    <div class="feature-block">
+      <span class="feature-title">Coding</span>
+      <div class="feature-content">
+      If you can write code for the web, you can write code for webOS!<br>Check out the <a href="https://sdk.webosarchive.org/" target="_top">SDK</a>, and reach out to the <a href="http://www.webosarchive.org/docs/community">Community</a> about how you can pitch in!
+      </div>  
+    </div>
+    
+    <div class="feature-block">
+      <span class="feature-title">Documenting and Archival</span>
+      <div class="feature-content">
+      Lots of content has been preserved by the Wayback Machine, but much of it needs updating, or re-working for modern times. <a href="https://github.com/webosarchive/webos-docs">Pull Requests</a> are welcome!
+      </div>  
+    </div>
+  </div>
+</div>
+
+<footer>
+<?php include("../legalese.php"); ?>
+</footer>
+</div>
+</body>
