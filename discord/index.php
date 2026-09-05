958428920d87d2430cfd893b242074a00fa7cd3f
codepoet80 <codepoet80@github.com>
2026-09-05 18:20:56 -0400

    save link

--- discord/index.php
+++ discord/index.php
@@ -1,6 +1,6 @@
 <?php
 if(!preg_match("@^https?://@", $_SERVER['REQUEST_URI'])) {
-        header('Location: https://discord.gg/tY2cAhbXXD');
+        header('Location: https://discord.gg/5VesdQpKFk');
 } else {
         header('Location: https://appcatalog.webosarchive.org/app/webOSSimpleChat');
 }
